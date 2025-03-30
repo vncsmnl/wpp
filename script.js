2c71277a6df29386e75e128a7a617e0a0718736d
Vinícius Manoel <59481808+vncsmnl@users.noreply.github.com>
2025-03-30 15:56:58 -0300

    :sparkles: feat: add some country codes

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 function redirecionar() {
     var telefone = document.getElementById('telefone').value;
+    var countryCode = document.getElementById('countryCode').value;
+
     if (telefone) {
-        window.location.href = `https://wa.me/55${telefone}`;
+        window.location.href = `https://wa.me/${countryCode}${telefone}`;
     } else {
         alert('Por favor, insira um número de telefone válido.');
     }
